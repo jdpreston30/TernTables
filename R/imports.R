d1efc71ecc395e3bb9d4de545b29bf10222f35d0
Josh Preston <jdpreston30@gmail.com>
2026-03-05 13:26:09 -0500

    ran devtools check on v1.6.3.9003, fixed note flagged, re-rendered vignette as HTML, updated manual.

--- R/imports.R
+++ R/imports.R
@@ -4,7 +4,7 @@
 #' @importFrom cli cli_rule cli_alert_info cli_alert_success cli_bullets cli_alert_warning
 #' @importFrom flextable flextable set_table_properties autofit width font fontsize bold italic bg align border_remove border body_add_flextable compose as_paragraph style padding height merge_at add_footer_lines hrule hline_top hline_bottom
 #' @importFrom officer read_docx body_add_par body_add_fpar body_add_break body_add_docx fpar ftext fp_text fp_par fp_border
-#' @importFrom stats chisq.test median na.omit quantile sd setNames
+#' @importFrom stats chisq.test median na.omit p.adjust quantile sd setNames
 #' @importFrom utils globalVariables
 #' @importFrom magrittr %>%
 #' @importFrom stringr str_extract str_trim str_to_lower str_to_title
